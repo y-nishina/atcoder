8b65d4f3f11ecff9e7bf84f406c8b0027c054d69
yuta nishina <y.nishina0509@gmail.com>
2021-01-05 23:24:28 +0900

    提出

---
+++ virtual/abc181/c/main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define repab(i, a, b) for (ll i = (ll)(a); i < (ll)(b); i++)
+#define repr(i, n) for (ll i = (ll)(n); i >= 0; i--)
+#define vsort(v) sort(v.begin(), v.end())
+#define vsortr(v) sort(v.begin(), v.end(), greater<ll>{})
+
+//定数
+#define INF32 2147483647           // 2.147483647×10^{9}:32bit整数のinf
+#define INF64 9223372036854775807  // 9.223372036854775807×10^{18}:64bit整数のinf
+#define MOD 1000000007             //問題による
+
+int main() {
+  ll n;
+  cin >> n;
+
+  vector<ll> x(n), y(n);
+  rep(i, n) cin >> x[i] >> y[i];
+
+  string ans = "No";
+  rep(i, n) {
+    for (ll j = i + 1; j < n; j++) {
+      for (ll k = j + 1; k < n; k++) {
+        if ((y[j] - y[i]) * (x[k] - x[i]) == (y[k] - y[i]) * (x[j] - x[i])) {
+          ans = "Yes";
+          break;
+        }
+      }
+    }
+  }
+
+  cout << ans << endl;
+}
