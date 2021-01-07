55ee1f1ab51d0ae72502235a88a188d5003b2155
yuta nishina <y.nishina0509@gmail.com>
2021-01-07 23:22:38 +0900

    不要な部分を削除して再提出

--- virtual/abc178/b/main.cpp
+++ virtual/abc178/b/main.cpp
@@ -16,14 +16,5 @@ int main() {
   ll a, b, c, d;
   cin >> a >> b >> c >> d;
 
-  ll m = max(max(a * c, a * d), max(b * c, b * d));
-
-  if (m < 0) {
-    if (a * b < 0 || c * d < 0) {
-      cout << 0 << endl;
-      return 0;
-    }
-  }
-
-  cout << m << endl;
+  cout << max(max(a * c, a * d), max(b * c, b * d)) << endl;
 }
