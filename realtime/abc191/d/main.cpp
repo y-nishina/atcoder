737a8263b101dbee75b6469881b900b59d7a5e91
yuta nishina <y.nishina0509@gmail.com>
2021-02-08 22:05:13 +0900

    一旦諦め

--- realtime/abc191/d/main.cpp
+++ realtime/abc191/d/main.cpp
@@ -2,6 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef long double ld;
 #define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
 #define repab(i, a, b) for (ll i = (ll)(a); i < (ll)(b); i++)
 #define repr(i, n) for (ll i = (ll)(n); i >= 0; i--)
@@ -12,18 +13,17 @@ typedef long long ll;
 #define INF32 2147483647           // 2.147483647×10^{9}:32bit整数のinf
 #define INF64 9223372036854775807  // 9.223372036854775807×10^{18}:64bit整数のinf
 #define MOD 1000000007             //問題による
+ld EPS = 1e-17;
 
 int main() {
-  double x, y, r;
+  ld x, y, r;
   cin >> x >> y >> r;
 
   ll ans = 0;
-  if (floor(x + r) - ceil(x - r) >= 0) {
-    repab(i, ceil(x - r), floor(x + r) + 1) {
-      ll b = ceil(y - sqrt((r + (i - x)) * (r - (i - x))));
-      ll t = floor(y + sqrt((r + (i - x)) * (r - (i - x))));
-      if (t - b >= 0) ans += t - b + 1;
-    }
+  repab(i, ceill(x - r - EPS), floorl(x + r + EPS) + 1) {
+    ll b = (ll)ceill(y - sqrtl(powl(r, 2) - powl((i - x), 2)) - EPS);
+    ll t = (ll)floorl(y + sqrtl(powl(r, 2) - powl((i - x), 2)) + EPS);
+    if (t - b >= 0) ans += t - b + 1;
   }
 
   cout << ans << endl;
