b74ba74595a3aaaa5652bd3f2521951190af7c16
yuta nishina <y.nishina0509@gmail.com>
2021-03-27 23:04:48 +0900

    コンテスト終了時点のコードをコミット

--- realtime/abc197/d/main.cpp
+++ realtime/abc197/d/main.cpp
@@ -2,6 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef long double ld;
 #define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
 #define repab(i, a, b) for (ll i = (ll)(a); i < (ll)(b); i++)
 #define repr(i, n) for (ll i = (ll)(n); i >= 0; i--)
@@ -14,10 +15,33 @@ typedef long long ll;
 #define MOD 1000000007             //問題による
 
 int main() {
-  ll n;
+  ld n;
   cin >> n;
+  ld x00, y00;
+  ld x0, y0, xn2, yn2;
+  cin >> x00 >> y00;
+  cin >> xn2 >> yn2;
 
-  ll ans = 0;
+  x0 = 0;
+  y0 = 0;
+  xn2 -= x00;
+  yn2 -= y00;
 
-  cout << ans << endl;
+  ld xdiff = xn2 - x0;
+  ld ydiff = yn2 - y0;
+
+  ld naikaku = (ld)180 * (n - 2) / n;
+  ld kyori2jo = (xdiff * xdiff) + (ydiff * ydiff);
+  ld kyori = (ld)sqrtl(kyori2jo);
+  ld hen = kyori * (ld)sinl(M_PI / n);
+  ld kakudo = -(naikaku / (ld)2 - (ld)(ydiff / xdiff));
+
+  ld ansx = hen * (ld)cosl(kakudo * M_PI / (ld)180);
+  ld ansy = hen * (ld)sinl(kakudo * M_PI / (ld)180);
+
+  ansx += x00;
+  ansy += y00;
+
+  cout << fixed << setprecision(10);
+  cout << ansx << ' ' << ansy << endl;
 }
