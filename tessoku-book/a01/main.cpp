42acf86093b2beb174cb75c29b5ede1849767488
yuta nishina <y.nishina0509@gmail.com>
2023-09-26 22:48:31 +0900

    commit

--- tessoku-book/a01/main.cpp
+++ tessoku-book/a01/main.cpp
@@ -8,16 +8,16 @@ typedef long long ll;
 #define vsort(v) sort(v.begin(), v.end())
 #define vsortr(v) sort(v.rbegin(), v.rend())
 
-//定数
+// 定数
 #define INF32 2147483647           // 2.147483647×10^{9}:32bit整数のinf
 #define INF64 9223372036854775807  // 9.223372036854775807×10^{18}:64bit整数のinf
-#define MOD 1000000007             //問題による
+#define MOD 1000000007             // 問題による
 
 int main() {
   ll n;
   cin >> n;
 
-  ll ans = 0;
+  ll ans = n * n;
 
   cout << ans << endl;
 }
